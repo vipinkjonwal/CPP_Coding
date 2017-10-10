7462f1b99afad43c96cfc884512c4886977d6584
Vipin Kumar <31645405+vipinkjonwal@users.noreply.github.com>
2017-10-10 15:30:58 +0000

    Typedef Usage

---
+++ typedef.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+typedef int meter;
+//typedef is used to create a new name for exisiting type.
+//Here type 'int' is renamed as 'meter'.
+
+int main(int argc, char const *argv[]){
+	meter kiloMeter=1;
+	cout << "There are 1000 meters in " << kiloMeter <<" Kilometer.";
+	return 0;
+}
