55ecacc00f4f3e569710d972cfd48819145cae29
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-11 21:38:37 +0530

    Variables Scope.

---
+++ scope.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int globalVariable;
+int localVariable = 25;
+
+int main(int argc, char const *argv[]){
+
+	cout << "Value of localVariable  is " << localVariable << endl;
+
+	int localVariable = 50;
+
+	int variableOne = 10;
+	int variableTwo = 20;
+
+	globalVariable = variableOne + variableTwo;
+
+	cout << "Sum of " << variableOne << " and " << variableTwo << " is " <<globalVariable << endl;
+	cout << "Value of localVariable  is " << localVariable << endl;
+
+	return 0;
+}
