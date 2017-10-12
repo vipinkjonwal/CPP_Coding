b24282859ad14bffed00501bbaafef44d2887708
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-12 21:17:56 +0530

    Constants and Literals

---
+++ constAndLiterals.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+#define PI 3.1415
+
+int main(int argc, char const *argv[]){
+	
+	int radius;
+	const int DIAMETER = 20; 
+	cout << "Please input the radius : ";
+	cin >> radius;
+	cout << "Area of first circle is " << PI*radius*radius << endl;
+	cout << "Perimeter of second circle is " << PI*DIAMETER;
+
+	/*
+	DIAMETER = 10;
+	cout << DIAMETER;
+	//It gives an error because we can't modify a read only variable created by const keyword.
+
+	*/
+
+	return 0;
+}
