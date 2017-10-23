f895fddc46c51867031a1dc526ad8cbc8d8df670
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-23 22:53:13 +0530

    Update helloWorld

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+//#include is used to include the header file iostream.
+
 using namespace std;
+//This line is used to specify that namespace standards are being used.
 
 int main(int argc, char const *argv[]){
+//This is main function whose return type is int.
+//It has two parameters, for command line usage.	
+	
 	cout << "Hello World!";
+	//cut is used to print what followed <<
+
 	return 0;
+	//main function is returning 0.
 }
