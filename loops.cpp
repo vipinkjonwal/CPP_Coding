f4d44529f5867fa959f6a3998f11f6aafc45cacf
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-26 22:26:09 +0530

    Loops in C++

--- loops.cpp
+++ loops.cpp
@@ -3,10 +3,43 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
 	
-	int sum = 0;
+	//for loop.
+	cout << "\n\tFOR LOOP\n";
+	int forSum = 0;
 	for (int i = 0; i < 10; ++i){
-		sum = sum + i;
+		cout << i << "th iteration.\n";
+		forSum = forSum + i;
+
+		if (forSum < 25){
+			continue;
+		}
+
+		else{
+			cout << "Sum is " << forSum << endl << endl;
+		}
 	}
-	cout << "Sum is" << sum;
+	cout << "Sum is " << forSum << endl << endl;
+
+	//while loop.
+	cout << "\n\tWHILE LOOP\n";
+	int myCounter = 10;
+	int whileSum = 0;
+	while (myCounter > 0){
+		cout << "Value of counter is " << myCounter << endl;
+		whileSum = whileSum + myCounter;
+		myCounter--;
+		cout << "Sum is " << whileSum << endl << endl;
+	}
+	cout<< "Sum is "<< whileSum << endl;
+
+	//do-while loop
+	cout << "\n\tDO-WHILE LOOP\n";
+	int doWhile = 3;
+	do{
+		cout << "Value of counter is " << doWhile << endl;
+		doWhile--;
+	}while(doWhile!=0);
+
+
 	return 0;
 }
