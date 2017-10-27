b74459874b15b73cbbd430a51ab11508814f64c0
SALONIKWATRA123 <33151263+SALONIKWATRA123@users.noreply.github.com>
2017-10-27 19:09:11 +0530

    Update constAndLiterals.cpp

--- constAndLiterals.cpp
+++ constAndLiterals.cpp
@@ -2,6 +2,9 @@
 #include <math.h>
 using namespace std;
 #define PI 3.1415
+/*If you define Pi using #define directive,you only need to make one change,in the #define directive itself.
+  #define direcrive can be used even to replace a condition like #define OR|| & to replace even an entire C or C++ statement.
+  For eg. #define TICK cout << "Welcome"
 
 int main(int argc, char const *argv[]){
 	
@@ -11,6 +14,7 @@ int main(int argc, char const *argv[]){
 	cin >> radius;
 	cout << "Area of first circle is " << PI*radius*radius << endl;
 	cout << "Perimeter of second circle is " << PI*DIAMETER;
+	
 
 	/*
 	
@@ -19,6 +23,8 @@ int main(int argc, char const *argv[]){
 	It gives an error because we can't modify a read only variable created by const keyword.
 
 	*/
+	
 
 	return 0;
+	
 }
