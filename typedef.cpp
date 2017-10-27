56e4cf6a91c4ebbd49bbc3bf0c3cdcee1890306c
SALONIKWATRA123 <33151263+SALONIKWATRA123@users.noreply.github.com>
2017-10-27 19:13:13 +0530

    Update typedef.cpp

--- typedef.cpp
+++ typedef.cpp
@@ -4,6 +4,7 @@ using namespace std;
 typedef int meter;
 //typedef is used to create a new name for exisiting type.
 //Here type 'int' is renamed as 'meter'.
+// If it is desirable to cause a defined name to become undefined, it can br accomplished by means of #undef directive.
 
 int main(int argc, char const *argv[]){
 	meter kiloMeter=1;
