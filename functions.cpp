fd3d288610691aac25a14d924db84853e1a52d6c
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-28 17:57:54 +0530

    Functions in C++

--- functions.cpp
+++ functions.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
+//This is function declaration, to avoid the overhead of forward referencing.
+int max(int,int);
+
+//This is the main function. Every program has a main function.
 int main(int argc, char const *argv[]){
 	
+	int num1;
+	int num2;
+	int maximum;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cout << "Enter another number: ";
+	cin >> num2;
+
+	//This is function calling, and the value returned is stored in variable maximum.
+	maximum = max(num1,num2);
+
+	cout << "Maximum of " << num1 << " and " << num2 <<" is "<<maximum;
 	return 0;
+}
+
+//This is function definition.
+int max(int num1,int num2){
+	/*
+	Objective 		: To find maximum of two numbers.
+	Parameters		: num1 - integer type, number 1
+					  num2 - integer type, number 2
+	Return value    : Maximum of two numbers.	 
+	*/
+	if (num1 > num2){
+		return num1;
+	}
+	else{
+		return num2;
+	}
 }
