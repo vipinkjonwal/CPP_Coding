19ed056391c4dfeb426afc1af3ad6fe745161d68
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-29 20:46:26 +0530

    Update enum.cpp

--- enum.cpp
+++ enum.cpp
@@ -7,32 +7,32 @@ enum Company{
 };
 
 int main(int argc, char const *argv[]){
-	
+
 	int choice;
-	repeat:																//repeat is a label.
-	cout << "Please enter a choice : ";
-	cin >> choice;
-	switch(choice){
-		case Google: 
-				cout << "You have chosen Google.";
-				break;
-		case Facebook: 
-				cout << "You have chosen Facebook.";
-				break;
-		case Twitter: 
-				cout << "You have chosen Twitter.";
-				break;
-		case Snapdeal: 
-				cout << "You have chosen Snapdeal.";
-				break;
-		case Microsoft: 
-				cout << "You have chosen Microsoft.";
-				break;
-		default:														//default is used when none of cases mentioned above is hitted.
-				cerr << "ERROR: Please enter a valid choice.\n";		//cerr is used to output an error message only. It works same as cout.
-				goto repeat;											//goto repeat is unconditional jump to labe repeat.
-			break;	
-	}
+	do {
+		cout << "Please enter a choice : ";
+		cin >> choice;
+		switch(choice){
+			case Google:
+					cout << "You have chosen Google.";
+					break;
+			case Facebook:
+					cout << "You have chosen Facebook.";
+					break;
+			case Twitter:
+					cout << "You have chosen Twitter.";
+					break;
+			case Snapdeal:
+					cout << "You have chosen Snapdeal.";
+					break;
+			case Microsoft:
+					cout << "You have chosen Microsoft.";
+					break;
+			default:														//default is used when none of cases mentioned above is hitted.
+					cerr << "ERROR: Please enter a valid choice. Enter again.\n";		//cerr is used to output an error message only. It works same as cout.
+					break;
+		}
+	}while (choice < 0 || choice > 5);
 
 	return 0;
 }
