39948ad6df94db3f507a8d29a854a45e297d075d
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-10-31 21:36:48 +0530

    scopeTest.cpp

--- scopeTest.cpp
+++ scopeTest.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void outerFunction(){
+void outerFunction() {
   int i = 10;
+  //Scope of this i remains in this function outerFunction() only.
   int j = 1;
 
   if (j == 1) {
     int i = 20;
+    //Scope of this i remains for this if block.
     cout << i << '\n';
   }
 
