4b3a25f05bb3e71a8c107ca700cc101d142f5b4f
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-02 21:36:14 +0530

    random.cpp

--- random.cpp
+++ random.cpp
@@ -10,3 +10,10 @@ int main(int argc, char const *argv[]) {
   cout << "Random Number generated is: " << myRandom;
   return 0;
 }
+
+/*
+stdlib.h is used for rand() function.
+time.h is used for time() cuntion to fetch system time.
+srand() is used to seed the random number.
+rand()%10 will return random numbers between 1-10.
+*/
