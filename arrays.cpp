0133f7836cba94469b8bcba692f6a4696bc67437
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-04 22:30:42 +0530

    arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -11,6 +11,7 @@ int main(int argc, char const *argv[]) {
     cout << firstArray[i] << '\t';
   }
 
+  //Initialisation of 2-D array as row major.
   int secondArray[5][2] = {1,2,3,4,5,6,7,8,9,0};
 
   cout << "\n\nOutput of secondArray: \n";
@@ -20,6 +21,19 @@ int main(int argc, char const *argv[]) {
     }
     cout << endl;
   }
+
+  //Initialisation of 2-D array (Another method)
+  int thirdArray[2][5] = {{1,2,3,4,5},{6,7,8,9,0}};
+
+  cout << "\n\nOutput of thirdArray: \n";
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      cout << thirdArray[i][j] << '\t';
+    }
+    cout << endl;
+  }
+
+
 
 
   return 0;
