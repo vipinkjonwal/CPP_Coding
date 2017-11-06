f93b364a2c8d8a179f34a45c1bf275987c51a99c
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-06 21:28:51 +0530

    Maximize the Sum

---
+++ arrayProblem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int maximum(int array[]) {
+  int tempMaximum = 0;
+  int index = 0;
+
+  while (array[index] != '\0') {
+    tempMaximum = tempMaximum + index*array[index];
+    index++;
+  }
+  return tempMaximum;
+}
+
+int main(int argc, char const *argv[]) {
+  int array[20];
+  int size;
+  int answer = 0;
+
+  cout << "Enter the size of array: ";
+  cin >> size;
+
+  cout << "Enter the elements of array: ";
+  for (int i = 0; i < size; ++i) {
+    cin >> array[i];
+  }
+
+  array[size] = '\0';
+
+  answer = maximum(array);
+  cout << "Maximum sum is " << answer;
+
+  return 0;
+}
