3a36785ddd6dc611943187c0e56d770d1aa09f45
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-09 23:44:03 +0530

    First Two Smallest of Array

--- firstTwoSmallest.cpp
+++ firstTwoSmallest.cpp
@@ -17,25 +17,26 @@ int main(int argc, char const *argv[]) {
   int tempMin;
   tempMin = array[0];
   for (int i = 0; i < size; ++i) {
-    if (tempMin < array[i]) {
+    if (tempMin > array[i]) {
       tempMin = array[i];
     }
   }
   firstSmallest = tempMin;
 
+
   int temp = firstSmallest;
   firstSmallest = array[0];
   array[0] = temp;
 
-  /*
+
   tempMin = array[1];
   for (int i = 1; i < size; ++i) {
-    if (tempMin < array[i]) {
+    if (tempMin > array[i]) {
       tempMin = array[i];
     }
   }
   secondSmallest = tempMin;
-  */
+  
   cout << firstSmallest << '\t' << secondSmallest;
   return 0;
 }
