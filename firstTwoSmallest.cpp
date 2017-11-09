b83caf9545907f22d31aaaf9c577ae4c039fc4fa
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-09 23:41:23 +0530

    First Two Smallest of Array

---
+++ firstTwoSmallest.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  int size;
+  int array[20];
+  int firstSmallest;
+  int secondSmallest;
+  cout << "Enter the size of array: ";
+  cin >> size;
+
+  cout << "Enter the elements: ";
+  for (int i = 0; i < size; ++i) {
+    cin >> array[i];
+  }
+
+  int tempMin;
+  tempMin = array[0];
+  for (int i = 0; i < size; ++i) {
+    if (tempMin < array[i]) {
+      tempMin = array[i];
+    }
+  }
+  firstSmallest = tempMin;
+
+  int temp = firstSmallest;
+  firstSmallest = array[0];
+  array[0] = temp;
+
+  /*
+  tempMin = array[1];
+  for (int i = 1; i < size; ++i) {
+    if (tempMin < array[i]) {
+      tempMin = array[i];
+    }
+  }
+  secondSmallest = tempMin;
+  */
+  cout << firstSmallest << '\t' << secondSmallest;
+  return 0;
+}
