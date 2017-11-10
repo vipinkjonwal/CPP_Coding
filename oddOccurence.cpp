4280d668bd9e2fedfd705ebcfe15120347d3cddd
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-10 22:29:16 +0530

    Number Occurring Odd Number of Times

---
+++ oddOccurence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int getOddOccurrence(int array[], int size)
+{
+     int i;
+     int result = 0;
+     for (i=0; i < size; ++i)
+        result = result ^ array[i];
+
+     return result;
+}
+
+int main(int argc, char const *argv[]) {
+  int array[20];
+  int size;
+  int odd;
+
+  cout << "Enter the size of array: ";
+  cin >> size;
+
+  cout << "Enter the elements of array: ";
+  for (int i = 0; i < size; ++i) {
+    cin >> array[i];
+  }
+
+  odd = getOddOccurrence(array,size);
+  cout << "Odd Occurence : " << odd;
+  return 0;
+}
