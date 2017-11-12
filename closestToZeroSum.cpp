d779f2424d1c9b56cdef9a7ae04056b47cd38e41
Vipin Kumar <vipin.mca17.du@gmail.com>
2017-11-12 22:37:26 +0530

    Two elements whose sum is closest to zero.

--- closestToZeroSum.cpp
+++ closestToZeroSum.cpp
@@ -27,6 +27,12 @@ int main(int argc, char const *argv[]) {
   cout << "Enter the size of array: ";
   cin >> size;
 
+  while (size < 2) {
+    cerr << "ERROR: Size must be greatest than 2.\n";
+    cout << "Enter the size of array: ";
+    cin >> size;
+  }
+
   cout << "Enter the elements of array: ";
   for (int i = 0; i < size; ++i) {
     cin >> array[i];
