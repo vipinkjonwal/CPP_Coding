32e4056354a8880253cf0df355db387a8f584e75
Vipin Kumar <vipin.mca17.du@gmail.com>
2018-03-15 20:16:10 +0530

    Consecutive Pairs

---
+++ consecutivePairs.cpp
@@ -0,0 +1,25 @@
+/*
+Program     : Check if successive pair of numbers in the stack is consecutive or not. (Q2 : Assignment 1)
+Author      : © Vipin Kumar
+Created on  : March 14, 2018 15:28 IST
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+bool isConsecutive (int number1, int number2) {
+	return abs (number1 - number2) == 1;
+}
+
+
+
+int main () {
+	return 0;
+}
